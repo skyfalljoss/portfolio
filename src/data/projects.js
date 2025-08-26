968d676de2d119809cf05b7a5a563d41f493a4ef
Phong Nguyen <skyfalljoss@gmail.com>
2025-08-25 22:52:00 -0400

    personalInfo 4 features

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,13 @@
+import chatbotImg from '../assets/images/chatbot.jpeg';
+import dashboardImg from '../assets/images/stock-dashboard.jpg';
+import newsAggregatorImg from '../assets/images/news-aggregator.jpg';
+
 export const projects = [
   {
     id: 1,
     title: "Stock Chatbot ",
     description: "Stock Chatbot is a full-stack AI-powered assistant designed for stock prediction, analysis, and conversational financial insights. The project combines deep learning (LSTM) for time series forecasting, a robust backend API, and an interactive Streamlit frontend, enabling users to ask questions, request predictions, and visualize stock trends in a user-friendly chat interface.",
-    image: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=500&h=300&fit=crop",
+    image: chatbotImg,
     githubUrl: "https://github.com/skyfalljoss/STOCK_CHATBOT",
     liveUrl: "https://ecommerce-demo.netlify.app",
     tech: ["Python", "Flask", "Spacy", "Keras/TensorFlow", "Pandas", "Streamlit"],
@@ -14,7 +18,7 @@ export const projects = [
     id: 2,
     title: "Finance Portfolio Dashboard",
     description: "This is a real-time portfolio management dashboard that allows users to track their stock investments and portfolio performance with live market data. The application features a Python/Flask backend that fetches real-time stock data from yfinance and a frontend built with HTML, CSS, and JavaScript for a responsive user experience.",
-    image: "https://images.unsplash.com/photo-1611224923853-80b023f02d71?w=500&h=300&fit=crop",
+    image: dashboardImg,
     githubUrl: "https://github.com/skyfalljoss/Dashboard",
     liveUrl: "https://task-manager-demo.netlify.app",
     tech: ["Python", "Flask", "SQLAlchemy", "Pandas", "yfinance", "HTML/CSS", "JavaScript"],
@@ -25,7 +29,7 @@ export const projects = [
     id: 3,
     title: "News Aggregator Websites",
     description: "his is a Django-based news aggregator that scrapes articles from various news sources, stores them in a database, and displays them on a modern, responsive website. The project utilizes BeautifulSoup for web scraping, Django for the backend, and Tailwindcss for the frontend design.",
-    image: "https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?w=500&h=300&fit=crop",
+    image: newsAggregatorImg,
     githubUrl: "https://github.com/skyfalljoss/news_aggregator/",
     liveUrl: "https://weather-dashboard-demo.netlify.app",
     tech: ["Python", "Django", "REST API", "MySQL", "BeautifulSoup", "TailwindCSS"],
@@ -40,7 +44,7 @@ export const projects = [
     githubUrl: "https://github.com/skyfalljoss/Shelf_steam",
     tech: ["C", "Makefile", "POSIX API", "Unix/Linux"],
     category: "Other",
-    featured: true
+    featured: false
   },
   {
     id: 5,
